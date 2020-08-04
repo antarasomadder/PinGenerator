117e2b60ef437060c8f9213cd498621a002d2b39
antarasomadder <67540854+antarasomadder@users.noreply.github.com>
2020-08-04 21:37:05 +0600

    pingenerator commit

---
+++ pin-matcher-master/test.js
@@ -0,0 +1,6 @@
+<p id="fourdigit"></p>
+<script>
+var fourdigitsrandom = Math.floor(1000 + Math.random() * 9000);
+document.getElementById("fourdigit").innerHTML=fourdigitsrandom;
+// alert(fourdigitsrandom);
+</script>
